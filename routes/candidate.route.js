de773913691afe791cea9c4646fe804bcfd6e0a7
Gaurav Nehra <gauravnehra07@gmail.com>
2020-04-15 08:57:02 +0530

    Routes Fix

--- routes/candidate.route.js
+++ routes/candidate.route.js
@@ -3,21 +3,21 @@ const router = express.Router()
 const candidate_controller = require("../controllers/candidate.controller")
 const auth = require("../middlewares/auth")
 
-router.post("/candidate/signup", candidate_controller.signup)
+router.post("/signup", candidate_controller.signup)
 
-router.post("/candidate/signin", candidate_controller.signin)
+router.post("/signin", candidate_controller.signin)
 
-router.post("/candidate/signout", candidate_controller.signout)
+router.post("/signout", candidate_controller.signout)
 
-router.post("/candidate/signoutall", candidate_controller.signoutall)
+router.post("/signoutall", candidate_controller.signoutall)
 
-router.post("/candidate/resetpassword", candidate_controller.resetpassword)
+router.post("/resetpassword", candidate_controller.resetpassword)
 
-router.post("/candidate/practicetest", candidate_controller.practicetest)
+router.get("/practicetest", candidate_controller.practicetest)
 
-router.post("/candidate/attempttest/:id", candidate_controller.attempttest)
+router.get("/test/:id", candidate_controller.attempttest)
 
-router.post("/candidate/updateprofile", candidate_controller.attempttest)
+router.post("/updateprofile", candidate_controller.updateprofile)
 
 router.get("/", candidate_controller.dashboard)
 
--- routes/company.route.js
+++ routes/company.route.js
@@ -3,21 +3,21 @@ const router = express.Router()
 const company_controller = require("../controllers/company.controller")
 const auth = require("../middlewares/auth")
 
-router.post("/company/signup", company_controller.signup)
+router.post("/signup", company_controller.signup)
 
-router.post("/company/signin", company_controller.signin)
+router.post("/signin", company_controller.signin)
 
-router.post("/company/signout", company_controller.signout)
+router.post("/signout", company_controller.signout)
 
-router.post("/company/signoutall", company_controller.signoutall)
+router.post("/signoutall", company_controller.signoutall)
 
-router.post("/company/resetpassword", company_controller.resetpassword)
+router.post("/resetpassword", company_controller.resetpassword)
 
-router.post("/company/createtest", company_controller.createtest)
+router.post("/createtest", company_controller.createtest)
 
-router.post("/company/deletetest", company_controller.deletetest)
+router.delete("/", company_controller.deletetest)
 
-router.post("/company/testresult/:id", company_controller.testresult)
+router.get("/testresult/:id", company_controller.testresult)
 
 router.get("/", company_controller.dashboard)
 
