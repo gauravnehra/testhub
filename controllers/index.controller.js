88816ed59191bfbccd3992c091ffa3fcf59fc16f
divyanshpurohit <46006439+divyanshpurohit@users.noreply.github.com>
2020-05-02 15:50:07 +0530

    Style:Candidate Dasboard

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -209,7 +209,7 @@ exports.companyProfile = async (req, res) => {
         })
         promises.push(profilePromise)
         Promise.all(promises).then(() => {
-            res.render("company_profile",{data,style:'company_profile.css'})
+            res.render("company_profile",{data,style:'company_profile.css',layout:'layout2.hbs'})
             
         }).catch(error => {
            
@@ -271,4 +271,8 @@ exports.viewAllTests = async (req, res) => {
 
 exports.invite = async (req, res) => {
     res.render ('invite',{style:'invite.css',layout:'layout2.hbs'})
+}
+
+exports.candidateDashboard = async (req, res) => {
+    res.render ('candidate_dashboard',{style:'candidate_dashboard.css'})
 }
